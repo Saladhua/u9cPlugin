340257d206adafc9d94e949962b8bed3f7da4da1
lihaichuan <3551768043@qq.com>
2022-12-03 10:26:06 +0800

    君华形态转换单

---
+++ Plugin/UIPlugIn/JUNHUA/TransferFormUIMainFormWebPartExtended.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using UFIDA.U9.ISV.CBO.Lot.Proxy;
+using UFIDA.U9.SCM.INV.TransferFormUIModel;
+using UFSoft.UBF.UI.ControlModel;
+using UFSoft.UBF.UI.Controls;
+using UFSoft.UBF.UI.Custom;
+using UFSoft.UBF.UI.IView;
+using UFSoft.UBF.UI.WebControls.Association;
+using UFSoft.UBF.UI.WebControls.Association.Adapter;
+using UFSoft.UBF.UI.WebControls.ClientCallBack;
+using UFSoft.UBF.Util.DataAccess;
+
+namespace YY.U9.Cust.LI.UIPlugIn
+{
+    /// <summary>
+    /// 形态转换
+    /// </summary>
+    class TransferFormUIMainFormWebPartExtended : ExtendedPartBase
+    {
+        private TransferFormUIMainFormWebPart _part;
+        /// <summary>
+        /// 批号通过sv服务生成
+        /// </summary>
+        /// <param name="part"></param>
+        /// <param name="args"></param>
+        public override void AfterRender(IPart part, EventArgs args)
+        {
+            base.AfterRender(part, args);
+            this._part = (part as TransferFormUIMainFormWebPart);
+            long n = (long)Math.Floor((new Random()).NextDouble() * 10000000D);
+            try
+            {
+                _part.Model.TransferForm_TransferFormLs_TransferFormSLs.Records.ToString();
+            }
+            catch (Exception)
+            {
+                return;
+                throw;
+            }
+            foreach (var item in _part.Model.TransferForm_TransferFormLs_TransferFormSLs.Records)
+            {
+                string longs = "";
+                string wide = "";
+                try
+                {
+                    longs = item["DescFlexSegments_PrivateDescSeg1"].ToString();
+                }
+                catch (Exception)
+                {
+                    longs = "";
+                }
+                try
+                {
+                    wide = item["DescFlexSegments_PrivateDescSeg2"].ToString();
+                }
+                catch (Exception)
+                {
+                    wide = "";
+                }
+                if (string.IsNullOrEmpty(longs) && string.IsNullOrEmpty(wide))
+                {
+                    return;
+                }
+                try
+                {
+                    //string see = item["LotInfo_LotCode"].ToString();
+                    //string item_id = item["ItemInfo_ItemID"].ToString();
+                    ////string see1 = item["LotInfo_LotMaster"].ToString();
+                    //longs = string.IsNullOrEmpty(longs) ? "" : item["DescFlexSegments_PrivateDescSeg1"].ToString();
+                    //wide = string.IsNullOrEmpty(wide) ? "" : item["DescFlexSegments_PrivateDescSeg2"].ToString();
+                    //string db = "InvDoc_TransferFormSL";
+                    //string dbname = "Lotinfo_lotcode";
+                    //item["LotInfo_LotCode"] = MiscRcvUIMainFormWebPartExtended.GetBatch(longs, wide, db, dbname);
+                    string 
+
+
+
+
+
+                    CommonCreateLotMasterSRVProxy lotMasterSRV = new CommonCreateLotMasterSRVProxy();
+                    List<CreateLotMasterDTOData> createLotMasterDTOData = new List<CreateLotMasterDTOData>();
+                    CreateLotMasterDTOData createLot = new CreateLotMasterDTOData();
+                    createLot.Item = new UFIDA.U9.CBO.Pub.Controller.CommonArchiveDataDTOData();
+                    createLot.Item.ID = (long)item["ItemInfo_ItemID"];
+                    createLot.Item.Name = item["ItemInfo_ItemName"].ToString();
+                    createLot.Item.Code = item["ItemInfo_ItemID"].ToString();
+                    createLot.LotCode = item["LotInfo_LotCode"].ToString();
+                    createLotMasterDTOData.Add(createLot);
+                    lotMasterSRV.CreateLotMasterDTOList = createLotMasterDTOData;
+                    //lotMasterSRV.Do();
+                    List<IDCodeNameDTOData> see2222 = lotMasterSRV.Do();
+                    foreach (var k in see2222)
+                    {
+                        item["LotInfo_LotMaster"] = k.ID;
+                    }
+                    item["InureDate"] = DateTime.Now.ToString("yyyy.MM.dd");
+                    item["LotInfo_DisabledDatetime"] = DateTime.Now.AddDays(+90).ToString("yyyy.MM.dd");
+
+                    #region 单中的计算
+                    string kg = "";
+                    DataTable dataTable = new DataTable();
+                    string sql = "SELECT DescFlexField_PrivateDescSeg6 FROM cbo_ItemMaster WHERE ID='" + item_id + "'";
+                    DataSet dataSet = new DataSet();
+                    DataAccessor.RunSQL(DataAccessor.GetConn(), sql, null, out dataSet);
+                    dataTable = dataSet.Tables[0];
+                    if (dataTable.Rows != null && dataTable.Rows.Count > 0)
+                    {
+                        kg = dataTable.Rows[0]["DescFlexField_PrivateDescSeg6"].ToString();
+                        if (string.IsNullOrEmpty(kg))
+                            kg = "0";
+                    }
+                    if (!string.IsNullOrEmpty(longs) && !string.IsNullOrEmpty(wide))//都有值
+                    {
+                        kg = Math.Round((decimal.Parse(longs) / 1000 * decimal.Parse(wide) / 1000) * decimal.Parse(kg), 3).ToString();
+                    }
+                    else if (!string.IsNullOrEmpty(longs) && string.IsNullOrEmpty(wide))//长有值，宽无值
+                    {
+                        kg = Math.Round(decimal.Parse(longs) / 1000 * decimal.Parse(kg), 3).ToString();
+                    }
+                    else if (string.IsNullOrEmpty(longs) && !string.IsNullOrEmpty(wide))//宽有值，长无值
+                    {
+                        kg = Math.Round(decimal.Parse(wide) / 1000 * decimal.Parse(kg), 3).ToString();
+                    }
+                    else
+                    {
+                        kg = "0";
+                    }
+                    #endregion
+                    string update1 = "UPDATE Lot_LotMaster SET DescFlexSegments_PrivateDescSeg1 = '" + longs + "', DescFlexSegments_PrivateDescSeg2 = '" + wide + "', DescFlexSegments_PrivateDescSeg3 = '" + kg + "' WHERE id = '" + item["LotInfo_LotMaster"] + "'";
+                    DataAccessor.RunSQL(DataAccessor.GetConn(), update1, null);
+
+                }
+                catch (Exception)
+                {
+                    item["LotInfo_LotMaster"] = n;
+                    longs = string.IsNullOrEmpty(longs) ? "" : item["DescFlexSegments_PrivateDescSeg1"].ToString();
+                    wide = string.IsNullOrEmpty(wide) ? "" : item["DescFlexSegments_PrivateDescSeg2"].ToString();
+                    string db = "InvDoc_TransferFormSL";
+                    string dbname = "Lotinfo_lotcode";
+                    item["LotInfo_LotCode"] = MiscRcvUIMainFormWebPartExtended.GetBatch(longs, wide, db, dbname);
+                }
+                string see2 = item["LotInfo_LotCode"].ToString();
+
+            }
+        }
+
+    }
+
+}
--- Plugin/UIPlugIn/TransferFormUIMainFormWebPartExtended.cs
+++ Plugin/UIPlugIn/TransferFormUIMainFormWebPartExtended.cs
@@ -1,188 +1,188 @@
-﻿using System;
-using System.Collections;
-using System.Data;
-using UFIDA.U9.SCM.INV.TransferFormUIModel;
-using UFSoft.UBF.UI.ControlModel;
-using UFSoft.UBF.UI.Controls;
-using UFSoft.UBF.UI.Custom;
-using UFSoft.UBF.UI.IView;
-using UFSoft.UBF.UI.WebControls.Association;
-using UFSoft.UBF.UI.WebControls.Association.Adapter;
-using UFSoft.UBF.UI.WebControls.ClientCallBack;
-using UFSoft.UBF.Util.DataAccess;
+﻿//using System;
+//using System.Collections;
+//using System.Data;
+//using UFIDA.U9.SCM.INV.TransferFormUIModel;
+//using UFSoft.UBF.UI.ControlModel;
+//using UFSoft.UBF.UI.Controls;
+//using UFSoft.UBF.UI.Custom;
+//using UFSoft.UBF.UI.IView;
+//using UFSoft.UBF.UI.WebControls.Association;
+//using UFSoft.UBF.UI.WebControls.Association.Adapter;
+//using UFSoft.UBF.UI.WebControls.ClientCallBack;
+//using UFSoft.UBF.Util.DataAccess;
 
-namespace YY.U9.Cust.LI.UIPlugIn
-{
-    // Token: 0x02000006 RID: 6
-    internal class TransferFormUIMainFormWebPartExtended : ExtendedPartBase
-    {
-        // Token: 0x04000004 RID: 4
-        private TransferFormUIMainFormWebPart _part;
-        // Token: 0x06000018 RID: 24 RVA: 0x00003F6C File Offset: 0x0000216C
-        public override void AfterInit(IPart part, EventArgs e)
-        {
-            base.AfterInit(part, e);
-            this._part = (part as TransferFormUIMainFormWebPart);
-            VersionChangedCallBack();
-            ItemChangedCallBack();
+//namespace YY.U9.Cust.LI.UIPlugIn
+//{
+//    // Token: 0x02000006 RID: 6
+//    internal class TransferFormUIMainFormWebPartExtended : ExtendedPartBase
+//    {
+//        // Token: 0x04000004 RID: 4
+//        private TransferFormUIMainFormWebPart _part;
+//        // Token: 0x06000018 RID: 24 RVA: 0x00003F6C File Offset: 0x0000216C
+//        public override void AfterInit(IPart part, EventArgs e)
+//        {
+//            base.AfterInit(part, e);
+//            this._part = (part as TransferFormUIMainFormWebPart);
+//            VersionChangedCallBack();
+//            ItemChangedCallBack();
 
 
-        }
+//        }
 
-        // Token: 0x06000019 RID: 25 RVA: 0x00003F84 File Offset: 0x00002184
-        public override void AfterRender(IPart Part, EventArgs args)
-        {
-            base.AfterRender(Part, args);
+//        // Token: 0x06000019 RID: 25 RVA: 0x00003F84 File Offset: 0x00002184
+//        public override void AfterRender(IPart Part, EventArgs args)
+//        {
+//            base.AfterRender(Part, args);
             
-        }
+//        }
 
-        ///<summary>
-        /// 注册表格单元格内容改变的回调事件
-        ///</summary>
-        private void VersionChangedCallBack()
-        {
-            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
+//        ///<summary>
+//        /// 注册表格单元格内容改变的回调事件
+//        ///</summary>
+//        private void VersionChangedCallBack()
+//        {
+//            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
 
-            AssociationControl gridCellDataChangedASC = new AssociationControl();       //基本固定代码
-            gridCellDataChangedASC.SourceServerControl = datagrid;
-            gridCellDataChangedASC.SourceControl.EventName = "OnCellDataChanged";
+//            AssociationControl gridCellDataChangedASC = new AssociationControl();       //基本固定代码
+//            gridCellDataChangedASC.SourceServerControl = datagrid;
+//            gridCellDataChangedASC.SourceControl.EventName = "OnCellDataChanged";
 
 
-            //CallBack处理方案
-            //版本
-            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("ItemVersion");
-            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("DescFlexSegments_PrivateDescSeg1");
+//            //CallBack处理方案
+//            //版本
+//            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("ItemVersion");
+//            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("DescFlexSegments_PrivateDescSeg1");
 
-            ClientCallBackFrm gridCellDataChangedCBF = new ClientCallBackFrm();
-            gridCellDataChangedCBF.ParameterControls.Add(datagrid);
+//            ClientCallBackFrm gridCellDataChangedCBF = new ClientCallBackFrm();
+//            gridCellDataChangedCBF.ParameterControls.Add(datagrid);
 
-            gridCellDataChangedCBF.DoCustomerAction += new ClientCallBackFrm.ActionCustomer(VersionChange);
-            gridCellDataChangedCBF.Add(gridCellDataChangedASC);
-            //this.Controls.Add(gridCellDataChangedCBF);
+//            gridCellDataChangedCBF.DoCustomerAction += new ClientCallBackFrm.ActionCustomer(VersionChange);
+//            gridCellDataChangedCBF.Add(gridCellDataChangedASC);
+//            //this.Controls.Add(gridCellDataChangedCBF);
 
-        }
-        ///<summary>
-        /// 表格的CallBack处理方式，返回结果
-        ///</summary>
-        ///<param name="args"></param>
-        ///<returns></returns>
-        private object VersionChange(CustomerActionEventArgs args)
-        {
-            this._part.OnDataCollect(this);//收集数据
-            this._part.IsDataBinding = true;
-            this._part.IsConsuming = false;
-            if (this._part.Model.ErrorMessage.hasErrorMessage)
-            {
-                this._part.Model.ClearErrorMessage();
-            }
-            //PR_PRLineListRecord record = this._part.Model.PR_PRLineList.FocusedRecord;
-            //string itemVersion = record.ItemVersion_Version;
+//        }
+//        ///<summary>
+//        /// 表格的CallBack处理方式，返回结果
+//        ///</summary>
+//        ///<param name="args"></param>
+//        ///<returns></returns>
+//        private object VersionChange(CustomerActionEventArgs args)
+//        {
+//            this._part.OnDataCollect(this);//收集数据
+//            this._part.IsDataBinding = true;
+//            this._part.IsConsuming = false;
+//            if (this._part.Model.ErrorMessage.hasErrorMessage)
+//            {
+//                this._part.Model.ClearErrorMessage();
+//            }
+//            //PR_PRLineListRecord record = this._part.Model.PR_PRLineList.FocusedRecord;
+//            //string itemVersion = record.ItemVersion_Version;
 
-            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
+//            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
 
-            UFWebClientGridAdapter grid = new UFWebClientGridAdapter(datagrid);
+//            UFWebClientGridAdapter grid = new UFWebClientGridAdapter(datagrid);
 
-            //取表格数据（当前行）
-            ArrayList list = (ArrayList)args.ArgsHash[UFWebClientGridAdapter.ALL_GRIDDATA_SelectedRows]; //基本固定代码
-            int curIndex = int.Parse(list[0].ToString());
-            Hashtable table = (Hashtable)((ArrayList)args.ArgsHash[datagrid.ClientID])[curIndex];
-            string paperVersion = "";
+//            //取表格数据（当前行）
+//            ArrayList list = (ArrayList)args.ArgsHash[UFWebClientGridAdapter.ALL_GRIDDATA_SelectedRows]; //基本固定代码
+//            int curIndex = int.Parse(list[0].ToString());
+//            Hashtable table = (Hashtable)((ArrayList)args.ArgsHash[datagrid.ClientID])[curIndex];
+//            string paperVersion = "";
 
-            string itemVersion = table["ItemInfo_ItemVersion"].ToString(); //版本
+//            string itemVersion = table["ItemInfo_ItemVersion"].ToString(); //版本
 
-            //匹配对应的图纸版本
-            //正式的ValueSetDef为 1002207040036824，测试的ValueSetDef为1002206131210010
+//            //匹配对应的图纸版本
+//            //正式的ValueSetDef为 1002207040036824，测试的ValueSetDef为1002206131210010
 
-            DataTable dataTable = new DataTable();
-            string version = "select  A.[ID], A.[Code], A1.[Name], A.[DependantCode] from  Base_DefineValue as A  left join [Base_DefineValue_Trl] as A1 on (A1.SysMlFlag = 'zh-CN') and (A.[ID] = A1.[ID])where(A.[ValueSetDef] = 1002207040036824)and a.Code = '" + itemVersion + "'";
-            DataSet dataSet = new DataSet();
-            DataAccessor.RunSQL(DataAccessor.GetConn(), version, null, out dataSet);
-            dataTable = dataSet.Tables[0];
+//            DataTable dataTable = new DataTable();
+//            string version = "select  A.[ID], A.[Code], A1.[Name], A.[DependantCode] from  Base_DefineValue as A  left join [Base_DefineValue_Trl] as A1 on (A1.SysMlFlag = 'zh-CN') and (A.[ID] = A1.[ID])where(A.[ValueSetDef] = 1002207040036824)and a.Code = '" + itemVersion + "'";
+//            DataSet dataSet = new DataSet();
+//            DataAccessor.RunSQL(DataAccessor.GetConn(), version, null, out dataSet);
+//            dataTable = dataSet.Tables[0];
 
             
-            if (dataTable != null && dataTable.Rows.Count > 0)
-            {
-                paperVersion = dataTable.Rows[0]["Code"].ToString();
-            }
+//            if (dataTable != null && dataTable.Rows.Count > 0)
+//            {
+//                paperVersion = dataTable.Rows[0]["Code"].ToString();
+//            }
              
-            //图纸版本
-            grid.CellValue.Add(new object[] { curIndex, "DescFlexSegments_PrivateDescSeg1_ID", new string[] { paperVersion, paperVersion, paperVersion } });
-            args.ArgsResult.Add(grid.ClientInstanceWithValue);
-            return args;
-        }
+//            //图纸版本
+//            grid.CellValue.Add(new object[] { curIndex, "DescFlexSegments_PrivateDescSeg1_ID", new string[] { paperVersion, paperVersion, paperVersion } });
+//            args.ArgsResult.Add(grid.ClientInstanceWithValue);
+//            return args;
+//        }
 
-        ///<summary>
-        /// 注册表格单元格内容改变的回调事件
-        ///</summary>
-        private void ItemChangedCallBack()
-        {
-            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
+//        ///<summary>
+//        /// 注册表格单元格内容改变的回调事件
+//        ///</summary>
+//        private void ItemChangedCallBack()
+//        {
+//            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
 
-            AssociationControl gridCellDataChangedASC = new AssociationControl();       //基本固定代码
-            gridCellDataChangedASC.SourceServerControl = datagrid;
-            gridCellDataChangedASC.SourceControl.EventName = "OnCellDataChanged";
-
-
-            //CallBack处理方案
-            //料号
-            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("ItemInfo_ItemID");
-            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("DescFlexSegments_PrivateDescSeg1");
-
-            ClientCallBackFrm gridCellDataChangedCBF = new ClientCallBackFrm();
-            gridCellDataChangedCBF.ParameterControls.Add(datagrid);
-
-            gridCellDataChangedCBF.DoCustomerAction += new ClientCallBackFrm.ActionCustomer(ItemChange);
-            gridCellDataChangedCBF.Add(gridCellDataChangedASC);
-            //this.Controls.Add(gridCellDataChangedCBF);
-
-        }
-        ///<summary>
-        /// 表格的CallBack处理方式，返回结果
-        ///</summary>
-        ///<param name="args"></param>
-        ///<returns></returns>
-        private object ItemChange(CustomerActionEventArgs args)
-        {
-            this._part.OnDataCollect(this);//收集数据
-            this._part.IsDataBinding = true;
-            this._part.IsConsuming = false;
-            if (this._part.Model.ErrorMessage.hasErrorMessage)
-            {
-                this._part.Model.ClearErrorMessage();
-            }
-            //PR_PRLineListRecord record = this._part.Model.PR_PRLineList.FocusedRecord;
-            //string itemVersion = record.ItemVersion_Version;
-
-            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
-
-            UFWebClientGridAdapter grid = new UFWebClientGridAdapter(datagrid);
-
-            //取表格数据（当前行）
-            ArrayList list = (ArrayList)args.ArgsHash[UFWebClientGridAdapter.ALL_GRIDDATA_SelectedRows]; //基本固定代码
-            int curIndex = int.Parse(list[0].ToString());
-            Hashtable table = (Hashtable)((ArrayList)args.ArgsHash[datagrid.ClientID])[curIndex];
-            string paperVersion = "";
-
-            string itemCode = table["ItemInfo_ItemID_Code"].ToString();
-
-            //料品私有段3，图纸版本值
-            DataTable dt = new DataTable();
-            string version_ = "select DescFlexField_PrivateDescSeg3 from CBO_ItemMaster where Code = '" + itemCode + "'";
-            DataSet ds = new DataSet();
-            DataAccessor.RunSQL(DataAccessor.GetConn(), version_, null, out ds);
-            dt = ds.Tables[0];
-
-            if (dt != null && dt.Rows.Count > 0)
-            {
-                paperVersion = dt.Rows[0]["DescFlexField_PrivateDescSeg3"].ToString();
-                //paperVersion = dt.Rows[0]["DescFlexField_PrivateDescSeg3"].ToString();
-            }
-            //图纸版本
-            grid.CellValue.Add(new object[] { curIndex, "DescFlexSegments_PrivateDescSeg1_ID", new string[] { paperVersion, paperVersion, paperVersion } });
-            args.ArgsResult.Add(grid.ClientInstanceWithValue);
-            return args;
-        }
+//            AssociationControl gridCellDataChangedASC = new AssociationControl();       //基本固定代码
+//            gridCellDataChangedASC.SourceServerControl = datagrid;
+//            gridCellDataChangedASC.SourceControl.EventName = "OnCellDataChanged";
 
 
-    }
-}
+//            //CallBack处理方案
+//            //料号
+//            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("ItemInfo_ItemID");
+//            ((IUFClientAssoGrid)gridCellDataChangedASC.SourceControl).FireEventCols.Add("DescFlexSegments_PrivateDescSeg1");
+
+//            ClientCallBackFrm gridCellDataChangedCBF = new ClientCallBackFrm();
+//            gridCellDataChangedCBF.ParameterControls.Add(datagrid);
+
+//            gridCellDataChangedCBF.DoCustomerAction += new ClientCallBackFrm.ActionCustomer(ItemChange);
+//            gridCellDataChangedCBF.Add(gridCellDataChangedASC);
+//            //this.Controls.Add(gridCellDataChangedCBF);
+
+//        }
+//        ///<summary>
+//        /// 表格的CallBack处理方式，返回结果
+//        ///</summary>
+//        ///<param name="args"></param>
+//        ///<returns></returns>
+//        private object ItemChange(CustomerActionEventArgs args)
+//        {
+//            this._part.OnDataCollect(this);//收集数据
+//            this._part.IsDataBinding = true;
+//            this._part.IsConsuming = false;
+//            if (this._part.Model.ErrorMessage.hasErrorMessage)
+//            {
+//                this._part.Model.ClearErrorMessage();
+//            }
+//            //PR_PRLineListRecord record = this._part.Model.PR_PRLineList.FocusedRecord;
+//            //string itemVersion = record.ItemVersion_Version;
+
+//            IUFDataGrid datagrid = (IUFDataGrid)_part.GetUFControlByName(_part.TopLevelContainer, "DataGrid8");
+
+//            UFWebClientGridAdapter grid = new UFWebClientGridAdapter(datagrid);
+
+//            //取表格数据（当前行）
+//            ArrayList list = (ArrayList)args.ArgsHash[UFWebClientGridAdapter.ALL_GRIDDATA_SelectedRows]; //基本固定代码
+//            int curIndex = int.Parse(list[0].ToString());
+//            Hashtable table = (Hashtable)((ArrayList)args.ArgsHash[datagrid.ClientID])[curIndex];
+//            string paperVersion = "";
+
+//            string itemCode = table["ItemInfo_ItemID_Code"].ToString();
+
+//            //料品私有段3，图纸版本值
+//            DataTable dt = new DataTable();
+//            string version_ = "select DescFlexField_PrivateDescSeg3 from CBO_ItemMaster where Code = '" + itemCode + "'";
+//            DataSet ds = new DataSet();
+//            DataAccessor.RunSQL(DataAccessor.GetConn(), version_, null, out ds);
+//            dt = ds.Tables[0];
+
+//            if (dt != null && dt.Rows.Count > 0)
+//            {
+//                paperVersion = dt.Rows[0]["DescFlexField_PrivateDescSeg3"].ToString();
+//                //paperVersion = dt.Rows[0]["DescFlexField_PrivateDescSeg3"].ToString();
+//            }
+//            //图纸版本
+//            grid.CellValue.Add(new object[] { curIndex, "DescFlexSegments_PrivateDescSeg1_ID", new string[] { paperVersion, paperVersion, paperVersion } });
+//            args.ArgsResult.Add(grid.ClientInstanceWithValue);
+//            return args;
+//        }
+
+
+//    }
+//}

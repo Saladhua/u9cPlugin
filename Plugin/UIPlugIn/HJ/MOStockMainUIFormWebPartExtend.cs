4a450b53dbb972dcc61f6f8986474f35aa957caa
lihaichuan <3551768043@qq.com>
2023-05-25 17:24:39 +0800

    宏巨配料计划按钮位置的修改

--- Plugin/UIPlugIn/HJ/MOStockMainUIFormWebPartExtend.cs
+++ Plugin/UIPlugIn/HJ/MOStockMainUIFormWebPartExtend.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Data;
 using UFIDA.U9.Base.SystemDock;
 using UFIDA.U9.Base.SystemDock.Proxy;
-using UFIDA.U9.MFG.MO.DiscreteMOUIModel;
+using UFIDA.U9.MFG.MO.DiscreteMOBListUIModel;
 using UFIDA.U9.UI.PDHelper;
 using UFSoft.UBF.UI.ControlModel;
 using UFSoft.UBF.UI.Custom;
@@ -11,12 +12,13 @@ using UFSoft.UBF.UI.IView;
 using UFSoft.UBF.UI.MD.Runtime;
 using UFSoft.UBF.UI.WebControlAdapter;
 using UFSoft.UBF.UI.WebControls;
+using UFSoft.UBF.Util.DataAccess;
 
 namespace YY.U9.Cust.LI.UIPlugIn
 {
     class MOStockMainUIFormWebPartExtend : ExtendedPartBase
     {
-        private MOStockMainUIFormWebPart _part;
+        private DiscreteMOBListUIFormWebPart _part;
         IUFMenu BtnSettle;
         /// <summary>
         /// 初始化后扩展
@@ -26,14 +28,14 @@ namespace YY.U9.Cust.LI.UIPlugIn
         public override void AfterInit(IPart part, System.EventArgs e)
         {
             base.AfterInit(part, e);
-            _part = part as MOStockMainUIFormWebPart;
+            _part = part as DiscreteMOBListUIFormWebPart;
             BtnSettle = new UFWebMenuAdapter();
             BtnSettle.Text = "配料";
             BtnSettle.ID = "BtnSettle";
             BtnSettle.AutoPostBack = true;
             BtnSettle.ItemClick += new MenuItemHandle(BtnSettle_Click);
             //加入操作里面
-            IUFDropDownButton DdbOperation = (IUFDropDownButton)_part.GetUFControlByName(part.TopLevelContainer, "DDBtnActivity");
+            IUFDropDownButton DdbOperation = (IUFDropDownButton)_part.GetUFControlByName(part.TopLevelContainer, "DDBtnOperation");
             DdbOperation.MenuItems.Add(BtnSettle);
         }
 
@@ -47,8 +49,12 @@ namespace YY.U9.Cust.LI.UIPlugIn
             this._part.OnDataCollect(this);
             try
             {
-                //IList<IUIRecord> selectRecords = _part.Model.MO_MOPickLists.);
-                NavigateManager.ShowModelWebpart(_part, "1498af39-8a9c-4179-9c2f-81716ad8630f", _part.TaskId.ToString(), 992, 504, null, true, true);
+
+                //IList<IUIRecord> selectRecords = _part.Model.MO_MOPickLists.);1498af39-8a9c-4179-9c2f-81716ad8630f
+                NavigateManager.NavigatePage(_part, "Cust.BatchingPlan");
+
+                //NavigateManager.ShowModelWebpart(_part, "1498af39-8a9c-4179-9c2f-81716ad8630f", _part.TaskId.ToString(), 992, 504, null, true, true);
+
                 GetPLMDocsProxy proxy = new GetPLMDocsProxy();
                 List<GetPLMDocInParaDTOData> list = new List<GetPLMDocInParaDTOData>();
                 DataTable dt = new DataTable();
@@ -64,27 +70,54 @@ namespace YY.U9.Cust.LI.UIPlugIn
                 dt.Columns.Add("BOMReqQty", typeof(string));//BOM需求数量
 
                 string docNo = "";
-                foreach (var item in _part.Model.MO.Records)
+                foreach (var item in _part.Model.MO.SelectRecords)
                 {
                     docNo = item["DocNo"].ToString();
-                }
 
-                foreach (IUIRecord record in _part.Model.MO_MOPickLists.SelectRecords)
-                {
                     DataRow dr = dt.NewRow();
                     dr["MOPickDocNO"] = docNo;
-                    dr["ItemInfo_ID"] = record["ItemMaster"];
-                    dr["ItemInfo_Code"] = record["ItemMaster_Code"];
-                    dr["ItemInfo_Name"] = record["ItemMaster_Name"];
+                    ///dr["ItemInfo_ID"] = item["MOPickLists_ItemMaster"];
+                    dr["ItemInfo_Code"] = item["MOPickLists_ItemMaster_Code"];
+                    dr["ItemInfo_Name"] = item["MOPickLists_ItemMaster_Name"];
+                    //dr["Project_ID"] = item["MOPickLists_Projects"];
+                    dr["Project_Name"] = item["MOPickLists_Project_Name"];
+                    dr["Project_Code"] = item["MOPickLists_Project_Code"];
+                    dr["BOMReqQty"] = item["MOPickLists_BOMReqQty"];
 
-                    dr["Project_ID"] = record["Project"];
-                    dr["Project_Name"] = record["Project_Name"];
-                    dr["Project_Code"] = record["Project_Code"]; 
 
-                    dr["BOMReqQty"] = record["BOMReqQty"];   
+                    DataTable dataTable = new DataTable();
+                    DataSet dataSet = new DataSet();
 
+                    string sqlForMoDocNoID = "SELECT Project,ItemMaster FROM MO_MOPickList WHERE MO=(SELECT ID FROM MO_MO WHERE DocNo='" + docNo + "')" +
+                        "   AND ItemMaster = (SELECT ID FROM CBO_ItemMaster WHERE Code = '" + item["MOPickLists_ItemMaster_Code"].ToString() + "' AND Org = '" + PDContext.Current.OrgID + "')";
+                    DataAccessor.RunSQL(DataAccessor.GetConn(), sqlForMoDocNoID, null, out dataSet);
+                    dataTable = dataSet.Tables[0];
+                    if (dataTable != null && dataTable.Rows.Count > 0)
+                    {
+                        dr["ItemInfo_ID"] = dataTable.Rows[0]["ItemMaster"].ToString();
+                        dr["Project_ID"] = dataTable.Rows[0]["Project"].ToString();
+                    }
                     dt.Rows.Add(dr);
                 }
+                #region 原来的
+                //foreach (IUIRecord record in _part.Model.MOPickList.SelectRecords)
+                //{
+                //    DataRow dr = dt.NewRow();
+                //    dr["MOPickDocNO"] = docNo;
+                //    dr["ItemInfo_ID"] = record["ItemMaster"];
+                //    dr["ItemInfo_Code"] = record["ItemMaster_Code"];
+                //    dr["ItemInfo_Name"] = record["ItemMaster_Name"];
+
+                //    dr["Project_ID"] = record["Project"];
+                //    dr["Project_Name"] = record["Project_Name"];
+                //    dr["Project_Code"] = record["Project_Code"]; 
+
+                //    dr["BOMReqQty"] = record["BOMReqQty"];   
+
+                //    dt.Rows.Add(dr);
+                //}
+
+                #endregion
                 this._part.CurrentSessionState["ResultMo"] = dt;
             }
             catch (Exception ex)
